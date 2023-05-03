33a81374673e934ea363d7ff74eed4efdb557fd0
keyamoni05866 <keya05866@gmail.com>
2023-05-03 21:50:03 +0600

    cors policy added

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 
 const chefs = require('./data/Chef.json')
+
+app.use(cors())
 app.get('/', (req, res) =>{
  res.send('Chef is coming soon')
 });
 
 
-app.get('/categories', (req, res) =>{
+app.get('/chefs', (req, res) =>{
     res.send(chefs)
 })
 app.listen(port, () =>{
