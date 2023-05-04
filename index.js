898fb25a03553262274d1876eb210f23a2c6e8df
keyamoni05866 <keya05866@gmail.com>
2023-05-04 15:25:12 +0600

    method updadted

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
+const chefs = require("./data/Chef.json");
 
-const chefs = require('./data/Chef.json')
-
-app.use(cors())
-app.get('/', (req, res) =>{
- res.send('Chef is coming soon')
+app.use(cors());
+app.get("/", (req, res) => {
+  res.send("Chef is coming soon");
 });
 
+app.get("/chefs", (req, res) => {
+  res.send(chefs);
+});
 
+app.get("/chefs/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedChefs = chefs.find((c) => c._id == id);
+  res.send(selectedChefs);
+});
 
-app.get('/chefs', (req, res) =>{
-    res.send(chefs)
-})
-
-app.get('/chefs/:id', (req, res) =>{
-    const id = req.params.id;
-    const selectedChefs= chefs.find( c => c._id == id);
-    res.send(selectedChefs)
-})
-
-app.listen(port, () =>{
-    console.log(`Chef API is running on port:${port}`)
-})
+app.listen(port, () => {
+  console.log(`Chef API is running on port:${port}`);
+});
